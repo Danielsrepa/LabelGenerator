403298da1ee59709131171763352134a94a9a7fd
Danielsrepa <daniels.liepins.vd@gmail.com>
2025-12-30 10:21:25 +0200

    letsee
    lets see

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/LabelGenerator/', // Matches your repo name: danielsrepa.github.io/LabelGenerator/
+  base: "/LabelGenerator/",
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false
-  }
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 });
